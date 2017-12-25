c716a44673fec209a9726f7fbc554df39b46087f
wangtonghe <wthfeng@126.com>
2017-12-25 19:25:56 +0800

    批量删除功能

--- src/main.vue
+++ src/main.vue
@@ -6,7 +6,7 @@
             <li role="presentation" v-link="{name: 'index'}"><a href="#" >数据查询与导出</a></li>
             <li role="presentation" v-link="{name:'dataImport'}"><a href="#" >数据导入</a></li>
             <li role="presentation" v-link="{name:'dataMigration'}"><a href="#">数据迁移</a></li>
-            <li role="presentation"><a href="#">批量删除</a></li>
+            <li role="presentation" v-link="{name:'bulkDelete'}"><a href="#" >批量删除</a></li>
             <li role="presentation"><a href="#">flyway删除</a></li>
             <li role="presentation"><a href="#">删除路由</a></li>
             <li role="presentation"><a href="#">数据导出·新</a></li>

95284182b0a54ed4887013d1882a6f2d9d236b15
scar <ghx0802@foxmail.com>
2017-12-26 19:11:43 +0800

    数据导入、数据迁移

--- src/views/DataImport.vue
+++ src/views/DataImport.vue
@@ -4,12 +4,12 @@
             <div role="form">
                 <div class="form-group">
                     <label for="">请在此填入数据</label>
-                    <textarea class="form-control" rows="10" placeholder="data"></textarea>
+                    <textarea id="importData" class="form-control" rows="5" placeholder="data"></textarea>
                 </div>
             </div>
-            <button type="button" class="btn btn-default">开始导入</button>
+            <button type="button" class="btn btn-default" @click="doImport">开始导入</button>
             <div class="result-panel panel panel-default">
-                <div id="exportResult" class="execResult panel-body">{{ message }}</div>
+                <div id="importResult" class="execResult panel-body">{{ message }}</div>
             </div>
         </div>
         <div class="page-content col-xs-3">
@@ -19,13 +19,72 @@
     </div>
 </template>
 <script>
+    import $ from 'jquery';
+    import utils from '../utils.js';
+
     export default {
         data(){
-            return {}
+            return {
+                index:'',
+                type:'',
+                message:'',
+                bulkBody:'',
+                types:[],
+                indexDict:{},
+
+            }
         },
-        computed: {},
-        components: {},
-        methods: {}
+        methods: {
+            doImport(){
+                let importData = $("#importData").val().trim();
+                if (!importData) {
+                    $("#importResult").html("参数为空");
+                    return false;
+                }
+
+                let importDataObj = JSON.parse(importData);
+                let dataBody;
+                if (typeof importDataObj.hits === "object" && !(importDataObj.hits instanceof Array) &&
+                    typeof importDataObj.hits.hits === "object" && importDataObj.hits.hits instanceof Array) {
+                    dataBody = importDataObj.hits.hits;
+                } else if (typeof importDataObj.hits === "object" && importDataObj.hits instanceof Array) {
+                    dataBody = importDataObj.hits;
+                }
+//                let bulkBody = "";
+                let i, dataItem, dataItemId, dataItemIndex, dataItemType, dataItemRouting, dataItemSource;
+                for (i = 0; i < dataBody.length; i++) {
+                    dataItem = dataBody[i];
+                    dataItemId = dataItem._id;
+                    dataItemIndex = dataItem._index;
+                    dataItemType = dataItem._type;
+                    dataItemRouting = dataItem._routing;
+                    dataItemSource = dataItem._source;
+
+                    if (dataItemRouting) {
+                        bulkBody += '{"index":{"_index":"' + dataItemIndex + '","_type":"' + dataItemType + '","_id":"' +
+                            dataItemId + '","_routing":"' + dataItemRouting + '"}}\n';
+                    } else {
+                        bulkBody += '{"index":{"_index":"' + dataItemIndex + '","_type":"' + dataItemType + '","_id":"' +
+                            dataItemId + '"}}\n';
+                    }
+                    bulkBody += JSON.stringify(dataItemSource) + '\n';
+                }
+
+                let importUrl = utils.getHost() + "/_bulk";
+                $.ajax({
+                    type: "POST",
+                    url: importUrl,
+                    data: bulkBody,
+                    success: function(result) {
+                        $("#importResult").html(format(JSON.stringify(result)), false);
+                    },
+                    error: function(result) {
+                        $("#importResult").html(JSON.stringify(result));
+                    }
+                });
+                return false;
+            }
+        }
     }
 </script>
 <style scoped>
--- src/views/DataMigration.vue
+++ src/views/DataMigration.vue
@@ -3,24 +3,24 @@
         <div class="page-content col-xs-9">
             <div role="form">
                 <div class="form-group">
-                    <label for="">起始索引</label>
+                    <label for="upgradeFromIndex">起始索引</label>
                     <div class="form-group">
-                        <select name="" class="form-control" v-model="index">
-                            <option v-for="index in indices" value="{{index}}">{{index}}</option>
+                        <select id="upgradeFromIndex" name="" class="form-control" v-model="index">
+                            <option v-for="indexBegin in indices" value="{{index}}">{{ indexBegin }}</option>
                         </select>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="">目标索引</label>
+                    <label for="upgradeToIndex">目标索引</label>
                     <div>
-                        <select name="" class="form-control">
-                            <option value="">hello world</option>
+                        <select id="upgradeToIndex" name="" class="form-control" v-model="index">
+                            <option v-for="indexEnd in indices" value="{{index}}">{{ indexEnd }}</option>
                         </select>
                     </div>
                 </div>
-                <button type="button" class="btn btn-default">开始迁移</button>
+                <button type="button" class="btn btn-default" @click="moveData">开始迁移</button>
                 <div class="result-panel panel panel-default">
-                    <div class="execResult panel-body">
+                    <div id="upgradeResult" class="execResult panel-body">
 
                     </div>
                 </div>
@@ -34,17 +34,163 @@
     </div>
 </template>
 <script>
+    import $ from 'jquery';
     import utils from '../utils';
     export default {
         data(){
             return {
                 indices:[],
                 index:'',
+                types:[],
+                message:'',
+                type:'',
+                indexDict:{}
             }
         },
-        computed: {},
-        components: {},
-        methods: {}
+
+        watch:{
+            index(val){
+                this.types=this.indexDict[val];
+                if(this.types.length>0){
+                    this.type=this.types[0]
+                }
+            }
+        },
+
+        ready() {
+            // 索引数据加载好之后处理索引select显示
+            $(document).on('dataReady', () => {
+                this.indexDict = utils.getState();
+                console.log('dataReady', this.indexDict);
+
+                let indices = [];
+                Object.keys(this.indexDict).forEach(index => {
+                    indices.push(index);
+                });
+                this.indices = indices;
+                if (this.indices.length > 0) {
+                    this.index = this.indices[0];
+                    this.types = this.indexDict[this.index];
+
+                    if (this.types.length > 0) {
+                        this.type = this.types[0];
+                    }
+                }
+
+            });
+        },
+
+        methods: {
+            moveData(){
+                let upgradeFromIndex = $("#upgradeFromIndex").val().trim();
+                let upgradeToIndex = $("#upgradeToIndex").val().trim();
+                if (!upgradeFromIndex || !upgradeToIndex) {
+                    $("#upgradeResult").html("参数为空");
+                    return false;
+                }
+
+                let upgradeQuery = function(results) {
+                    try {
+                        let importDataObj = results;
+                        let dataBody;
+                        if (typeof importDataObj.hits === "object" && !(importDataObj.hits instanceof Array) &&
+                            typeof importDataObj.hits.hits === "object" && importDataObj.hits.hits instanceof Array) {
+                            dataBody = importDataObj.hits.hits;
+                        } else if (typeof importDataObj.hits === "object" && importDataObj.hits instanceof Array) {
+                            dataBody = importDataObj.hits;
+                        }
+                        let bulkBody = "";
+                        let i, dataItem, dataItemId, dataItemIndex, dataItemType, dataItemRouting, dataItemSource;
+                        for (i = 0; i < dataBody.length; i++) {
+                            dataItem = dataBody[i];
+                            dataItemId = dataItem._id;
+                            dataItemIndex = upgradeToIndex;
+                            dataItemType = dataItem._type;
+                            dataItemRouting = dataItem._routing;
+                            dataItemSource = dataItem._source;
+
+                            if (dataItemRouting) {
+                                bulkBody += '{"index":{"_index":"' + dataItemIndex + '","_type":"' + dataItemType + '","_id":"' +
+                                    dataItemId + '","_routing":"' + dataItemRouting + '"}}\n';
+                            } else {
+                                bulkBody += '{"index":{"_index":"' + dataItemIndex + '","_type":"' + dataItemType + '","_id":"' +
+                                    dataItemId + '"}}\n';
+                            }
+                            bulkBody += JSON.stringify(dataItemSource) + '\n';
+                        }
+
+                        let importUrl = utils.getHost() + "/_bulk";
+                        $.ajax({
+                            type: "POST",
+                            url: importUrl,
+                            data: bulkBody,
+                            success: function(result) {
+                                totalSucc += dataBody.length;
+                                $("#upgradeResult").html("需要升级总数：" + totalHits + "    已升级：" + totalSucc);
+                                if (errMsg.length > 0) $("#upgradeResult").html("升级中发生错误：" + errMsg + "    需要升级总数：" + totalHits + "    已升级：" + totalSucc);
+                            },
+                            error: function(result) {
+                                errMsg += 'ts    ';
+                                msgExportNResult(errMsg);
+                                console.error('error', ts, e);
+                            }
+                        });
+                    } catch (err) {
+                        errMsg += err.message + '    ';
+                        $("#upgradeResult").html(errMsg);
+                        console.error(err.message, err.stack);
+                    }
+                };
+
+                let exportUrl = utils.getHost() + "/" + upgradeFromIndex;
+                exportUrl += "/_search?scroll=1m";
+                let postData = '{"fields":["_parent","_source"],"from":0,"size":10000,"query":{"bool":{"must":[],"must_not":[],' +
+                    '"should":[{"match_all":{}}]}},"sort":[],"aggs":{},"version":true}';
+                let nr = "...";
+                let totalHits = NaN;
+                let totalFrom = NaN;
+                let totalSucc = NaN;
+                let errMsg = '';
+                let ajaxQuery = function(scrollId) {
+                    if (!totalFrom) totalFrom = 0;
+                    if (scrollId) {
+                        exportUrl = utils.getHost() + '/_search/scroll?scroll=1m&scroll_id=' + scrollId;
+                        postData = ''
+                    }
+                    $.ajax({
+                        type: "POST",
+                        url: exportUrl,
+                        data: postData,
+                        success: function(result) {
+                            $("#upgradeResult").html(nr);
+                            nr += '.';
+                            if (!totalHits) {
+                                totalHits = result.hits.total;
+                                totalSucc = 0;
+                                $("#upgradeResult").html("需要升级总数：" + totalHits + "    已升级：" + 0);
+                            }
+                            if (typeof result.hits.hits === 'object' && result.hits.hits instanceof Array) {
+                                // console.log('ret5');
+                                // console.log(result.hits.hits.slice(0, 5));
+                                // exportResult.hits = exportResult.hits.concat(result.hits.hits);
+                                upgradeQuery(result);
+                            }
+                            if (totalFrom + 10000 < totalHits) {
+                                totalFrom += 10000;
+                                ajaxQuery(result._scroll_id);
+                            }
+                        },
+                        error: function(xhr, ts, e) {
+                            errMsg += 'ts    ';
+                            $("#upgradeResult").html(errMsg);
+                            console.error('error', ts, e);
+                        }
+                    });
+                };
+                ajaxQuery();
+                return false;
+            }
+        }
     }
 </script>
 <style scoped>
